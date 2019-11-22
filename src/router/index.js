1a48fc89c22bf2987c85909784cb05884a5f518f
aierlan <1227185612@qq.com>
2019-11-22 09:25:55 +0800

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,46 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/home/Home'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+	{
+		path: '/',
+		name: 'home',
+		component: Home
+	},
+	{
+		path: '/cateory',
+		name: 'cateory',
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () =>
+			import(/* webpackChunkName: "about" */ '../views/category/Category')
+	},
+	{
+		path: '/cart',
+		name: 'cart',
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () =>
+			import(/* webpackChunkName: "about" */ '../views/cart/Cart')
+	},
+	{
+		path: '/profile',
+		name: 'profile',
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () =>
+			import(/* webpackChunkName: "about" */ '../views/profile/Profile')
+	}
 ]
 
 const router = new VueRouter({
-  routes
+	routes
 })
 
 export default router
